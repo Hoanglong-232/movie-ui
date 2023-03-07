6b78212184eed2f0797c0c1a23fdd81033d9f09d
“Your <you@example.com>
2023-03-07 14:47:08 +0700

    update code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,56 @@
 import React, { Fragment, useState } from 'react';
 import { HiSearch } from 'react-icons/hi';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import '../Styles/Navbar.css';
+import Movies from './Movies';
+import TvShows from './TvShows';
+import Trending from './Trends';
+import Pricing from './Pricing';
 
 function Navbar() {
     const [toggle, setToggle] = useState(true);
 
     return (
         <Fragment>
-            <nav className="">
+            <nav className={toggle ? '' : 'navBarColor'}>
                 <div className="nav-options">
                     <h1 id={toggle ? '' : 'heading'}>REACTFILM</h1>
-                    <span id={toggle ? 'Movies' : 'MoviesLight'}>Movies</span>
-                    <span id={toggle ? 'Movies' : 'MoviesLight'}>TvShows</span>
-                    <span id={toggle ? 'Movies' : 'MoviesLight'}>Trending</span>
-                    <span id={toggle ? 'Movies' : 'MoviesLight'}>Pricing</span>
+
+                    <NavLink
+                        to=""
+                        style={({ isActive }) => {
+                            return { color: isActive ? '#fff' : '#ee9b00' };
+                        }}
+                    >
+                        <span id={toggle ? 'Movies' : 'MoviesLight'}>Movies</span>
+                    </NavLink>
+
+                    <NavLink
+                        to="/TvShows"
+                        style={({ isActive }) => {
+                            return { color: isActive ? '#fff' : '#ee9b00' };
+                        }}
+                    >
+                        <span id={toggle ? 'Movies' : 'MoviesLight'}>TvShows</span>
+                    </NavLink>
+
+                    <NavLink
+                        to="/Trending"
+                        style={({ isActive }) => {
+                            return { color: isActive ? '#fff' : '#ee9b00' };
+                        }}
+                    >
+                        <span id={toggle ? 'Movies' : 'MoviesLight'}>Trending</span>
+                    </NavLink>
+
+                    <NavLink
+                        to="/Pricing"
+                        style={({ isActive }) => {
+                            return { color: isActive ? '#fff' : '#ee9b00' };
+                        }}
+                    >
+                        <span id={toggle ? 'Movies' : 'MoviesLight'}>Pricing</span>
+                    </NavLink>
                 </div>
 
                 <div className="input-group">
@@ -24,6 +61,13 @@ function Navbar() {
                     </div>
                 </div>
             </nav>
+
+            <Routes>
+                <Route path="" element={<Movies />} />
+                <Route path="TvShows" element={<TvShows />} />
+                <Route path="Trending" element={<Trending />} />
+                <Route path="Pricing" element={<Pricing />} />
+            </Routes>
         </Fragment>
     );
 }
